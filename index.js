615f79aa650a48e5893ed69ffad34e6d35912c7d
ZoneTwelve <zonetwelve159@gmail.com>
2022-06-08 03:59:37 +0800

    [update] with basic componment

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+const path = require("path");
+
+const database = require("csv-database");
+
+function application( params ){
+  let { filePath, fileds, standDir } = params;
+
+}
+
+function resolvePath( ){
+
+}
+
+
+module.exports = application;
