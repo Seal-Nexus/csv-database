b8f27a2bd0f67498bb532bced720756d133f252b
ZoneTwelve <zonetwelve159@gmail.com>
2022-06-08 15:38:00 +0800

    [update] vaild data checking

--- index.js
+++ index.js
@@ -1,16 +1,101 @@
 "use strict";
+// System modules
 const path = require("path");
 
+// Third-party modules
 const database = require("csv-database");
 
 function application( params ){
   let { filePath, fileds, standDir } = params;
+  // filed system config
+  // NN = Not Null
+  // PK = Primary Key
+  // U  = Unique Key
+  // AI = Auto Increment
+
+  this.inputFileds = fileds;
+  this.filedKeys = fileds.map( v=>v.name );
+  this.fileds = new Object();
+
+  this.standDir = standDir;
+  this.filePath = filePath;
+  this.dbs = new Object( ); // Path: new csv-dataase
+  this.write = Write;
+
+  var getdb = getdb;
+  this.resolveDatabasePath = resolveDatabasePath;
+
+  // Init
+  for(let item of fileds){
+    this.fileds[item.name] = item;
+  }
+}
+
+function Read( ){
 
 }
 
-function resolvePath( ){
+function Write( data ){
+  for(let key of this.filedKeys ){
+    let d = data[key];
+    let filed = this.fileds[key];
+    // vaild data checking 
+    let dataCheckResult = dataChecking( filed, d );
+    if(dataCheckResult !== true)
+      return dataCheckResult;
+    
+    let db = this.getdb( d );
+  }
+
+  return { message:"ok" };
+}
+
+function Update( ){
 
 }
 
+function Delete( ){
+
+}
+
+function getdb( data ){
+  let filePath = this.filePath;
+  let dbPath = resolveDatabasePath( filePath, data );
+  let db = this.dbs[filePath];
+  if( db )
+    return db;
+}
+
+// common functions
+function dataChecking( filed, data ){
+  if( data === undefined ){
+    data = autofillValue( filed );
+  }
+
+  if( checkEmptyRule( filed ) === true && data === undefined ){
+    return { error:"data is missing" };
+  }
+  // Pass the data checking
+  //if( typeof data !== filed['type'] ){
+  //  return { error:"Type is not matching", key };
+  //}
+
+  return true;
+}
+
+function autofillValue( filed ){
+  if( typeof filed['autofill'] === "function" ){
+    return filed['autofill']();
+  }else if( filed['default'] !== undefined ){
+    return filed['default'];
+  }
+  return undefined;
+}
+
+function checkEmptyRule( filed ){
+  return filed['NN'] === true || 
+         filed['PK'] === true || 
+         filed['U']  === true;
+}
 
 module.exports = application;
---
+++ test/main.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const path = require("path");
+const database = require("../index");
+
+let db = new database({
+  //filePath: "$pid/$date/data.csv",
+  filePath: "hello/world/database.csv",
+  standDir: path.resolve( __dirname, "database" ),
+  fileds: [
+    { name: "pid",  type: "string" },
+    { name: "hr",   type: "string" },
+    { name: "spo2", type: "string" },
+    { name: "date", type: "string" }, // user input date
+    { name: "sys",  type: "string", autofill: ( ) => `${Number(new Date())}` }, // system time
+  ]
+});
+
+let res = db.write({
+  pid : "p001",
+  hr  : `${~~(Math.random()*30) + 70}`,
+  spo2: "99",
+  date: `${Number(new Date())}`,
+});
+
+console.log( res );
